d53d23aa42997fcc4454693998b5c6db2543c296
biyori-t <takuro.toji@gmail.com>
2022-07-14 23:17:08 +0900

    Todo の作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo';
+  task: string = '';
+  tasks = ['task1', 'task2', 'task3'];
+
+  addTask() {
+    if (this.task == '') {
+      return;
+    }
+
+    this.tasks.push(this.task);
+    this.task = '';
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
